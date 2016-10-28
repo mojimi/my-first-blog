5d5b63dad615f071b859e7d30d1bc25af0966133
Mojimi <ppgab.mail@gmail.com>
2016-10-28 16:46:38 -0200

    my django girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\6036794\documents\django\djangogirls\myvenv\scripts\python35.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
